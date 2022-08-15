4da39b8ec0d98b32838935d768c1546d5d3d49c9
casperklein <casperklein@users.noreply.github.com>
2022-08-15 20:37:09 +0200

    Remove -it flag

--- scan.sh
+++ scan.sh
@@ -8,4 +8,4 @@ DIR=${0%/*}
 cd "$DIR"
 
 echo "Starting Netbox-Scanner.."
-docker run "$@" --rm -it -v "$PWD"/netbox-scanner.conf:/root/.netbox-scanner.conf:ro -v "$PWD"/networks.txt:/netbox-scanner/networks.txt:ro "$TAG"
+docker run "$@" --rm -v "$PWD"/netbox-scanner.conf:/root/.netbox-scanner.conf:ro -v "$PWD"/networks.txt:/netbox-scanner/networks.txt:ro "$TAG"
